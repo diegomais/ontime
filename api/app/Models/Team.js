5a496c973c7763f9a1a02d25d94b0308961484e0
Diego Mais <diegomais.dev@gmail.com>
2020-03-30 23:22:30 -0300

    [feat](api) Add unique slugs when saving teams to database Generating unique slugs is a hard problem, since each application has unique requirements and constraints. So instead of defining a single strategy to generate slugs, the add-on Adonis Lucid Slugify keeps it flexible to define custom strategies.
    Changes to be committed:
    	modified:   app/Models/Team.js
    	modified:   package.json
    	modified:   start/app.js
    	modified:   yarn.lock

--- api/app/Models/Team.js
+++ api/app/Models/Team.js
@@ -1,6 +1,16 @@
 const Model = use('Model');
 
 class Team extends Model {
+  static boot() {
+    super.boot();
+
+    this.addTrait('@provider:Lucid/Slugify', {
+      fields: { slug: 'name' },
+      strategy: 'dbIncrement',
+      disableUpdates: false,
+    });
+  }
+
   users() {
     return this.belongsToMany('App/Models/User').pivotModel(
       'App/Models/UserTeam'
