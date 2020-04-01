9c5f4b1c80263a49f1c68d68e8cd31ee0855280b
Diego Mais <diegomais.dev@gmail.com>
2020-03-31 22:36:38 -0300

    [feat](api) Add CRUD for teams route
    Changes to be committed:
    	modified:   app/Controllers/Http/TeamController.js
    	modified:   start/routes.js

--- api/app/Controllers/Http/TeamController.js
+++ api/app/Controllers/Http/TeamController.js
@@ -1,93 +1,66 @@
-'use strict'
-
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-
 /**
  * Resourceful controller for interacting with teams
  */
 class TeamController {
   /**
-   * Show a list of all teams.
+   * Show a list of all teams from user.
    * GET teams
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
+  async index({ auth }) {
+    const teams = await auth.user.teams().fetch();
 
-  /**
-   * Render a form to be used for creating a new team.
-   * GET teams/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    return teams;
   }
 
   /**
    * Create/save a new team.
    * POST teams
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, auth }) {
+    const data = request.only(['name']);
+
+    const team = await auth.user.teams().create({
+      ...data,
+      user_id: auth.user.id,
+    });
+
+    return team;
   }
 
   /**
    * Display a single team.
    * GET teams/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
+  async show({ params, auth }) {
+    const team = await auth.user.teams().where('teams.id', params.id).first();
 
-  /**
-   * Render a form to update an existing team.
-   * GET teams/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+    return team;
   }
 
   /**
    * Update team details.
    * PUT or PATCH teams/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params, request, auth }) {
+    const data = request.only(['name']);
+    const team = await auth.user.teams().where('teams.id', params.id).first();
+
+    team.merge(data);
+
+    await team.save();
+
+    return team;
   }
 
   /**
    * Delete a team with id.
    * DELETE teams/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params, auth }) {
+    const team = await auth.user.teams().where('teams.id', params.id).first();
+
+    await team.delete();
   }
 }
 
-module.exports = TeamController
+module.exports = TeamController;
--- api/start/routes.js
+++ api/start/routes.js
@@ -15,3 +15,7 @@
 const Route = use('Route');
 
 Route.post('sessions', 'SessionController.store');
+
+Route.group(() => {
+  Route.resource('teams', 'TeamController').apiOnly();
+}).middleware('auth');
