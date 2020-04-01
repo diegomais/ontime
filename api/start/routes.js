6fd425d4263e218e81c525d69a713db15ae08267
Diego Mais <diegomais.dev@gmail.com>
2020-03-31 22:18:55 -0300

    [feat](api) Add authentication route
    Changes to be committed:
    	new file:   app/Controllers/Http/SessionController.js
    	modified:   start/routes.js

--- api/start/routes.js
+++ api/start/routes.js
@@ -1,5 +1,3 @@
-'use strict'
-
 /*
 |--------------------------------------------------------------------------
 | Routes
@@ -14,8 +12,6 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use('Route');
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('sessions', 'SessionController.store');
