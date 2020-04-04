40c4014368c16d969c74b6383afef71923b1de21
Diego Mais <diegomais.dev@gmail.com>
2020-04-04 13:06:50 -0300

    [feat](api) Add permissions route This route shows permissions for the authenticated user on the current team
    Changes to be committed:
    	new file:   app/Controllers/Http/PermissionController.js
    	modified:   start/routes.js

--- api/start/routes.js
+++ api/start/routes.js
@@ -42,4 +42,6 @@ Route.group(() => {
   Route.put('members/:id', 'MemberController.update').middleware(
     'is:administrator'
   );
+
+  Route.get('permissions', 'PermissionController.show');
 }).middleware(['auth', 'team']);
