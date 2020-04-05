a7250bebf41c94e7d7d01f0160f378ecaca9beeb
Diego Mais <diegomais.dev@gmail.com>
2020-04-05 00:44:52 -0300

    [feat](web) Add Sign In page UI
    Changes to be committed:
    	modified:   src/pages/SignIn/index.js
    	new file:   src/pages/SignIn/styles.js
    	new file:   src/styles/components/Button.js

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import Button from '~/styles/components/Button';
+import { Container, SignForm } from './styles';
 
 export default function SignIn() {
   return (
-    <div>
-      <h1>SignIn</h1>
-    </div>
+    <Container>
+      <SignForm onSubmit={() => {}}>
+        <h1>Welcome</h1>
+
+        <span>E-mail</span>
+        <input type="email" name="email" />
+
+        <span>Password</span>
+        <input type="password" name="password" />
+
+        <Button type="submit" size="big">
+          Sign In
+        </Button>
+      </SignForm>
+    </Container>
   );
 }
---
+++ web/src/pages/SignIn/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #202225;
+  height: 100%;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SignForm = styled.form`
+  background: #36393f;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.2);
+  padding: 40px;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  h1 {
+    font-size: 26px;
+    font-weight: 700;
+    text-align: center;
+    margin: 0 0 10px;
+  }
+
+  span {
+    color: #b9bbbe;
+    font-size: 14px;
+    line-height: 16px;
+    font-weight: 600;
+    margin-top: 15px;
+  }
+
+  input {
+    height: 40px;
+    padding: 10px;
+    border-radius: 3px;
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    background-color: 1px solid rgba(0, 0, 0, 0.1);
+    color: #666;
+    margin-top: 8px;
+    font-size: 16px;
+    transition: border 0.15s ease;
+
+    &:focus {
+      border-color: #7289da;
+    }
+  }
+
+  button {
+    margin-top: 20px;
+  }
+`;
