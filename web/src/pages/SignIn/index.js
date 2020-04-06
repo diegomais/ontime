2965222c6e9d3c15f610edbe8d6e46dbdb461e44
Diego Mais <diegomais.dev@gmail.com>
2020-04-05 21:38:56 -0300

    [feat](web) Add sign in request
    Changes to be committed:
    	modified:   package.json
    	modified:   src/pages/SignIn/index.js
    	new file:   src/services/api.js
    	modified:   src/store/modules/auth/actions.js
    	modified:   src/store/modules/auth/reducer.js
    	modified:   src/store/modules/auth/sagas.js
    	modified:   yarn.lock

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
+import { signInRequest } from '~/store/modules/auth/actions';
 import Button from '~/styles/components/Button';
 import { Container, SignForm } from './styles';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    dispatch(signInRequest(email, password));
+  }
+
   return (
     <Container>
-      <SignForm onSubmit={() => {}}>
+      <SignForm onSubmit={handleSubmit}>
         <h1>Welcome</h1>
 
         <span>E-mail</span>
-        <input type="email" name="email" />
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <span>Password</span>
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <Button type="submit" size="big">
           Sign In
--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -1,3 +1,26 @@
-import { all } from 'redux-saga/effects';
+import { call, put, all, takeLatest } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', { email, password });
+
+    const { token } = response.data;
+
+    localStorage.setItem('@OnTime:token', token);
+
+    yield put(signInSuccess(token));
+
+    history.push('/dashboard');
+  } catch (error) {
+    console.tron.log(error);
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
