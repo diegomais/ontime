f6f17d6d1a49bd49229ab89383d373b2fd1456f3
Diego Mais <diegomais.dev@gmail.com>
2020-04-10 10:25:29 -0300

    [fix](web) Fix modal width function Width needs 'px' in the end of props function
    Changes to be committed:
    	modified:   src/components/Modal/styles.js

--- web/src/components/Modal/styles.js
+++ web/src/components/Modal/styles.js
@@ -18,7 +18,7 @@ export const Content = styled.div`
   border-radius: 5px;
   box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.2);
   padding: 40px;
-  width: ${(props) => (props.size === 'big' ? 600 : 400)};
+  width: ${(props) => (props.size === 'big' ? 600 : 400)}px;
 
   h1 {
     font-size: 26px;
